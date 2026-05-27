f1c689d1b18640445213650740b07cd6ac143367
Shyamendra Hazra <shyamendrahazra@gmail.com>
2026-05-27 10:38:02 +0530

    code: crate user model and export it

--- packages/database/models/user.ts
+++ packages/database/models/user.ts
@@ -1,1 +1,12 @@
+import { pgTable, uuid, timestamp, text, varchar } from "drizzle-orm/pg-core";
 
+export const userTable = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+
+    fullName: varchar("full_name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    passwordHash: text("password_hash"),
+
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
